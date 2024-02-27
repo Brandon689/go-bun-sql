55fa754e373542640c4ff14d13f7357d7a92eb17
Brandon <brandon139@protonmail.com>
2024-02-27 18:33:01 +1100

    first

--- cmd/main.go
+++ cmd/main.go
@@ -51,30 +51,3 @@ func main() {
 
 	fmt.Printf("User ID: %d, Name: %s, Email: %s\n", user.ID, user.Name, user.Email)
 }
-
-//package main
-//
-//import (
-//	"database/sql"
-//	"fmt"
-//	"github.com/uptrace/bun"
-//	"github.com/uptrace/bun/driver/sqliteshim"
-//
-//	"github.com/uptrace/bun"
-//	"github.com/uptrace/bun/dialect/sqlitedialect"
-//)
-//
-//func main() {
-//	sqldb, err := sql.Open(sqliteshim.ShimName, "file::memory:?cache=shared")
-//	if err != nil {
-//		panic(err)
-//	}
-//	db := bun.NewDB(sqldb, sqlitedialect.New())
-//
-//	res, err := db.ExecContext(ctx, "SELECT 1")
-//	fmt.Println(res)
-//	var num int
-//	err2 := db.QueryRowContext(ctx, "SELECT 1").Scan(&num)
-//	fmt.Println(err2)
-//
-//}
